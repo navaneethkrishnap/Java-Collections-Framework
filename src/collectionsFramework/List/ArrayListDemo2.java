2226c341c2083b41dd8de1061d2f3497e04f698f
Navaneeth Krishna P <navneeth10p@gmail.com>
2025-10-09 15:21:36 +0530

    new files added

--- src/collectionsFramework/List/ArrayListDemo2.java
+++ src/collectionsFramework/List/ArrayListDemo2.java
@@ -1,4 +1,43 @@
 package collectionsFramework.List;
 
 public class ArrayListDemo2 {
+    public static void main(String[] args) {
+
+        ArrayList<Character> ex1 = new ArrayList<Character>();
+
+        ex1.add('N');
+        ex1.add('A');
+        ex1.add('v');
+        ex1.add('A');
+        ex1.add('K');
+        ex1.add('I');
+
+        System.out.println(ex1);
+
+        ArrayList<Character> ex1_addAll = new ArrayList<Character>();
+
+        ex1_addAll.addAll(ex1);
+        System.out.println("This is copy");
+        System.out.println(ex1_addAll) ;
+        // remove
+        System.out.println("After removing all the elements from the copy");
+        ex1_addAll.removeAll(ex1);
+        System.out.println(ex1_addAll);
+
+
+        // sort
+        System.out.println("before sorting");
+        System.out.println("Elements in the ex1: "+ ex1);
+        System.out.println("Elements after sorting: ");
+        Collections.sort(ex1_addAll);
+        System.out.println(ex1);
+        System.out.println("Sort in reverse order");
+        Collections.sort(ex1, Collections.reverseOrder());
+        System.out.println(ex1);
+
+        // Shuffle
+        Collections.shuffle(ex1);
+        System.out.println("Elements in the ex1 after shuffling: "+ex1);
+
+    }
 }
